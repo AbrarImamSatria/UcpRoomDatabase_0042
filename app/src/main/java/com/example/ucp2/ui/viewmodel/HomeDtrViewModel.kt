43ce20d49a909b3efa8c228caa1c7e4b0a27ef34
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-20 15:00:55 +0700

    membuat Implementasi HomeDtrViewModel dengan StateFlow untuk mengelola state UI

--- app/src/main/java/com/example/ucp2/ui/viewmodel/HomeDtrViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/HomeDtrViewModel.kt
@@ -1,6 +1,52 @@
 package com.example.ucp2.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.Dokter
+import com.example.ucp2.repository.LocalRepositoryDtr
+import com.example.ucp2.repository.RepositoryDtr
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeDtrViewModel(
+    private val repositoryDtr: RepositoryDtr
+) : ViewModel(){
+
+    val homeDtrUiState: StateFlow<HomeDtrUiState> = repositoryDtr.getAllDtr()
+        .filterNotNull()
+        .map {
+            HomeDtrUiState(
+                listDtr = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeDtrUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeDtrUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeDtrUiState(
+                isLoading = true,
+            )
+        )
+}
 
 data class HomeDtrUiState (
     val listDtr: List<Dokter> = listOf(),
