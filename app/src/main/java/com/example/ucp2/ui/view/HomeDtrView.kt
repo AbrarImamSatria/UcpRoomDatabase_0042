3b23e362eb4b47d084ccc4b1c275e37b9d39608c
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-20 18:16:56 +0700

    membuat function BodyHomeDtrView

--- app/src/main/java/com/example/ucp2/ui/view/HomeDtrView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/HomeDtrView.kt
@@ -1,8 +1,10 @@
 package com.example.ucp2.ui.view
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -11,17 +13,84 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2.data.entity.Dokter
+import com.example.ucp2.ui.viewmodel.HomeDtrUiState
+import kotlinx.coroutines.launch
 
+@Composable
+fun BodyHomeDtrView (
+    homeDtrUiState: HomeDtrUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() } //Snackbar state
+    when {
+        homeDtrUiState.isLoading -> {
+            //Menampilkan indikator loading
+            Box (
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        homeDtrUiState.isError -> {
+            //Menampilkan pesan error
+            LaunchedEffect(homeDtrUiState.errorMessage) {
+                homeDtrUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message) //Tampilkan Snackbar
+                    }
+                }
+            }
+        }
+
+        homeDtrUiState.listDtr.isEmpty() -> {
+            //Menampilkan pesan jika data kosong
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Tidak ada data dokter.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            //Menampilkan daftar mahasiswa
+            ListDokter(
+                listDtr = homeDtrUiState.listDtr,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
 
 @Composable
 fun ListDokter(
