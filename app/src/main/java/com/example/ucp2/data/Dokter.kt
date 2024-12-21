e9cb7c0c020d4f2612808b3430ce438637d0aa36
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-21 15:18:23 +0700

    Membuat object Dokter yang berisi pilihan spesialis dokter

---
+++ app/src/main/java/com/example/ucp2/data/Dokter.kt
@@ -0,0 +1,11 @@
+package com.example.ucp2.data
+
+object Dokter {
+    val options = listOf(
+        "Dokter Spesialis Kandungan",
+        "Dokter Spesialis Anak",
+        "Dokter Umum",
+        "Dokter Spesialis Penyakit dalam",
+        "Dokter Spesialis Neurologi",
+    )
+}
