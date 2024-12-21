1f2ae1aebfe052ae53d89da59fd17a0a1671e3c6
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-21 13:30:51 +0700

    membuat fun savedata dan validasidata

--- app/src/main/java/com/example/ucp2/ui/viewmodel/InsertHomeDtrView.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/InsertHomeDtrView.kt
@@ -1,5 +1,66 @@
 package com.example.ucp2.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Dokter
+import com.example.ucp2.repository.RepositoryDtr
+import kotlinx.coroutines.launch
+
+class DokterViewModel(
+    private val repositoryDtr: RepositoryDtr
+) : ViewModel() {
+
+    var uiState by mutableStateOf(DtrUIState())
+
+    //validasi data input pengguna
+    fun validateFields(): Boolean {
+        val event = uiState.dokterEvent
+        val errorState = FormErrorState(
+            id = if (event.id.isNotEmpty()) null else "NIM tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            spesialis = if (event.spesialis.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+            klinik = if (event.klinik.isNotEmpty()) null else "Alamat tidak boleh kosong",
+            noHp = if (event.noHp.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            jamKerja = if (event.jamKerja.isNotEmpty()) null else "Angkatan tidak boleh kosong"
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    //menyimpan data ke repository
+    fun saveData() {
+        val currentEvent = uiState.dokterEvent
+
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    repositoryDtr.insertDtr(currentEvent.toDokterEntity())
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data berhasil disimpan",
+                        dokterEvent = DokterEvent(), // Reset input form
+                        isEntryValid = FormErrorState() // Reset error state
+                    )
+                } catch (e: Exception) {
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data gagal disimpan"
+                    )
+                }
+            }
+        } else {
+            uiState = uiState.copy(
+                snackBarMessage = "Input tidak valid. Periksa kembali data Anda."
+            )
+        }
+    }
+    // Reset pesan Snackbar setelah ditampilkan
+    fun resetSnackBarMessage() {
+        uiState = uiState.copy(snackBarMessage = null)
+    }
+}
+
 data class DtrUIState(
     val dokterEvent: DokterEvent = DokterEvent(),
     val isEntryValid: FormErrorState = FormErrorState(),
