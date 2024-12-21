fc6b6a39cc57c3734d1a9d187a43e8b564882663
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-21 20:29:25 +0700

    membuat class HomeJwlViewModel

--- app/src/main/java/com/example/ucp2/ui/viewmodel/HomeJwlViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/HomeJwlViewModel.kt
@@ -1,9 +1,54 @@
 package com.example.ucp2.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.Jadwal
+import com.example.ucp2.repository.RepositoryJwl
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeJwlViewModel (
+    private val repositoryJwl: RepositoryJwl
+) : ViewModel() {
+
+    val homeUIState: StateFlow<HomeUiState> = repositoryJwl.getAllJwl()
+        .filterNotNull()
+        .map {
+            HomeUiState (
+                listJwl = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState (
+                isLoading = true,
+            )
+        )
+}
 
 data class HomeUiState (
-    val listMhs: List<Jadwal> = listOf(),
+    val listJwl: List<Jadwal> = listOf(),
     val isLoading: Boolean = false,
     val isError: Boolean = false,
     val errorMessage: String = ""
