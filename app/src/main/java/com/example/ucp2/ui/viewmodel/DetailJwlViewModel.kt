6ad6892930c69edb75ae78f04c85673f55425900
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-22 17:21:53 +0700

    membuat data class DetailUiState

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DetailJwlViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.ucp2.ui.viewmodel
+
+data class DetailUiState (
+    val detailUiEvent: JadwalEvent = JadwalEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == JadwalEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != JadwalEvent()
+}
