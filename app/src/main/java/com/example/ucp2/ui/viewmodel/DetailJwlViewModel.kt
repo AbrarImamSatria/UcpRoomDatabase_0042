acfd9da8ba1c13fc8f4a583cc20547cbecc1b8f7
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-22 17:46:05 +0700

    membuat fun jadwal untuk memindahkan data dari entity ke ui

--- app/src/main/java/com/example/ucp2/ui/viewmodel/DetailJwlViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DetailJwlViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.ucp2.ui.viewmodel
 
+import com.example.ucp2.data.entity.Jadwal
+
 data class DetailUiState (
     val detailUiEvent: JadwalEvent = JadwalEvent(),
     val isLoading: Boolean = false,
@@ -11,4 +13,19 @@ data class DetailUiState (
 
     val isUiEventNotEmpty: Boolean
         get() = detailUiEvent != JadwalEvent()
+}
+
+/*
+* Data class untuk menampung data yang akan ditampilkan di UI*/
+
+//memindahkan data dari entity ke ui
+fun Jadwal.toDetailUiEvent() : JadwalEvent {
+    return JadwalEvent(
+        id = id,
+        namaDokter = namaDokter,
+        namaPasien = namaPasien,
+        noHp = noHp,
+        tanggalKonsultasi = tanggalKonsultasi,
+        status = status
+    )
 }
