e2ad35dff98564a154f93022430b3050baef1a97
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-22 16:56:55 +0700

    memperbaiki entity jadwal

--- app/src/main/java/com/example/ucp2/data/entity/Jadwal.kt
+++ app/src/main/java/com/example/ucp2/data/entity/Jadwal.kt
@@ -4,19 +4,10 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "jadwal",
-    foreignKeys = [ForeignKey(
-        entity = Dokter::class,
-        parentColumns = arrayOf("id"),
-        childColumns = arrayOf("idDokter"),
-        onUpdate = ForeignKey.CASCADE,
-        onDelete = ForeignKey.CASCADE
-    )]
-)
+@Entity(tableName = "jadwal")
 data class Jadwal(
-    @PrimaryKey
-    val id: String,
-    val idDokter: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
     val namaDokter: String,
     val namaPasien: String,
     val noHp: String,
