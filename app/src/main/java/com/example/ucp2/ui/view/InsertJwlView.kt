5153c9f38cab1888a4949c0b2932f270499c02d6
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-22 15:55:53 +0700

    membuat fun InsertJwlView serta DestinasiInsertJadwal

--- app/src/main/java/com/example/ucp2/ui/view/InsertJwlView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/InsertJwlView.kt
@@ -4,18 +4,102 @@ package com.example.ucp2.ui.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.costumwidget.TopAppBar
+import com.example.ucp2.ui.navigation.AlamatNavigasi
 import com.example.ucp2.ui.viewmodel.FormErrorStateJwl
 import com.example.ucp2.ui.viewmodel.JadwalEvent
+import com.example.ucp2.ui.viewmodel.JadwalViewModel
 import com.example.ucp2.ui.viewmodel.JwlUIState
+import com.example.ucp2.ui.viewmodel.PenyediaViewModel
 import com.example.ucp2.ui.widget.DynamicSelectTextField
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+object DestinasiInsertJadwal: AlamatNavigasi {
+    override val route: String = "insert_mhs"
+}
+
+@Composable
+fun InsertJwlView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: JadwalViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val uiState = viewModel.JwluiState // Ambil UI State dari ViewModel
+    val dokterList = viewModel.dokterList // Ambil daftar dokter
+    val snackbarHostState = remember { SnackbarHostState() } //Snackbar State
+    val coroutineScope = rememberCoroutineScope()
+
+    //Observasi perubahan snackbarMessage
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message) //Tampilkan Snackbar
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold (
+        modifier = Modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            TopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Jadwal"
+            )
+        }
+    ){ padding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ){
+            // Isi Body
+            InsertBodyJwl(
+                uiState = uiState,
+                dokterList = dokterList.map { it.nama },
+                onValueChange = {updatedEvent ->
+                    viewModel.updateState(updatedEvent)
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.validateFields()) {
+                            viewModel.saveData()
+                            delay(600)
+                            withContext(Dispatchers.Main) {
+                                onNavigate()
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
 
 @Composable
 fun InsertBodyJwl(
