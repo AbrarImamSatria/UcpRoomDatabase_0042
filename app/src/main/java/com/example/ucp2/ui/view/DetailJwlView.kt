b1039c075c8be4672ce39a7b92c6198278b41db0
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-22 17:53:52 +0700

    membuat fun ItemDetailJwl

--- app/src/main/java/com/example/ucp2/ui/view/DetailJwlView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/DetailJwlView.kt
@@ -1,8 +1,13 @@
 package com.example.ucp2.ui.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -10,7 +15,40 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ucp2.data.entity.Jadwal
+
+@Composable
+fun ItemDetailJwl(
+    modifier: Modifier = Modifier,
+    jadwal: Jadwal
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailJwl(judul = "Id : ", isinya = jadwal.id.toString())
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJwl(judul = "Nama Pasien : ", isinya = jadwal.namaPasien)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJwl(judul = "Nama Dokter : ", isinya = jadwal.namaDokter)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJwl(judul = "Nomor Hp : ", isinya = jadwal.noHp)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJwl(judul = "Tanggal : ", isinya = jadwal.tanggalKonsultasi)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJwl(judul = "Status Penanganan", isinya = jadwal.status)
+        }
+    }
+}
 
 @Composable
 fun ComponentDetailJwl(
